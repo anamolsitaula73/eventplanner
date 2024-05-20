b1973ac54a94216b51617db721e628ea4b3886b8
anamolsitaula73 <anmolsitaula73@gmail.com>
2024-05-20 22:30:35 +0545

    second commit with html file

--- eventplanner/views.py
+++ eventplanner/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def aboutUS(request):
     return HttpResponse("Welcome to EG")
 def home(request):
-    return HttpResponse("<b>THis is home</b>") 
+    return render(request,"index.html") 
 def homeNav(request,name):
     return HttpResponse("<b>Hello</b>   "+name) 
+def about(request):
+    return HttpResponse("hi this is features")
+def login(request):
+    return render(request,"login.html") 
+def signup(request):
+    return render(request,"signup.html") 
